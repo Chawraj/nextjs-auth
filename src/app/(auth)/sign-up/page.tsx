c91b89b709a75c3f7204967fbae25b576cdb0e4f
Chawraj <p.chawraj@gmail.com>
2023-09-03 22:26:19 +0200

    first comit
    first

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import SignUpForm from "@/components/form/SignUpForm"
+
+
+const page = () => {
+  return (
+    <div className="w-full">
+        <SignUpForm></SignUpForm>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/UserAccountnav.tsx
@@ -0,0 +1,17 @@
+ 'use client';
+
+import { signOut } from "next-auth/react";
+import { Button } from "./ui/button";
+
+const UserAccountnav = () => {
+  return (
+    <Button onClick={()=> signOut({
+        redirect: true,
+        callbackUrl: `${window.location.origin}/sign-in`,
+    })} variant="destructive">
+        Sign out
+    </Button>
+  )
+}
+
+export default UserAccountnav
